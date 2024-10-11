4981e6ed2f908721860498afafba266da2efb028
Mike Wazowsky <yatoz0r@gmail.com>
2024-10-11 12:35:27 +0500

    aboba epope v1

--- IS lab01/CryptLogics/DiffieHellman.cs
+++ IS lab01/CryptLogics/DiffieHellman.cs
@@ -1,15 +1,16 @@
 ﻿
 using System.Numerics;
 using System.Security.Cryptography;
+using System.Text;
 
 namespace IS_lab01.CryptLogics
 {
     public class DiffieHellman
     {
+        public BigInteger N { get; private set; }
+        public BigInteger Q { get; private set; }
         public BigInteger X { get; private set; }
         public BigInteger Y { get; private set; }
-        public BigInteger ClientPrivateA { get; private set; }
-        public BigInteger ClientPrivateB { get; private set; }
 
         public BigInteger PublicKeyA { get; private set; }
         public BigInteger PublicKeyB { get; private set; }
@@ -22,21 +23,21 @@ namespace IS_lab01.CryptLogics
 
         public DiffieHellman()
         {
+            N = GenerateRandomNumber();
+
+            Q = GenerateRandomNumber();
+
             X = GenerateRandomNumber();
 
             Y = GenerateRandomNumber();
-
-            ClientPrivateA = GenerateRandomNumber();
-
-            ClientPrivateB = GenerateRandomNumber();
             CalculateKeys();
         }
         
         private void CalculateKeys()
         {
-            PublicKeyA = BigInteger.ModPow(Y, ClientPrivateA, X);
-            PublicKeyB = BigInteger.ModPow(Y, ClientPrivateB, X);
-            _privateKey = BigInteger.ModPow(PublicKeyB, PublicKeyB, X);
+            PublicKeyA = BigInteger.ModPow(Q, X, N);
+            PublicKeyB = BigInteger.ModPow(Q, Y, N);
+            _privateKey = BigInteger.ModPow(PublicKeyB, PublicKeyB, N);
         }
         private bool IsPrime(BigInteger number)
         {
@@ -67,11 +68,10 @@ namespace IS_lab01.CryptLogics
         public string Encrypt(string plaintext)
         {
             string ciphertext = "";
-            double tangent = Math.Tan((double)_privateKey);
 
             foreach (char c in plaintext)
             {
-                BigInteger encryptedChar = BigInteger.Multiply((BigInteger)c, _privateKey)/* * (BigInteger)tangent*/;
+                BigInteger encryptedChar = BigInteger.Multiply((BigInteger)c, _privateKey);
                 ciphertext += encryptedChar.ToString() + " ";
             }
 
@@ -82,15 +82,15 @@ namespace IS_lab01.CryptLogics
         {
             string plaintext = "";
             string[] encryptedChars = ciphertext.Trim().Split(' ');
-            double tangent = Math.Tan((double)_privateKey); 
             foreach (string encryptedChar in encryptedChars)
             {
                 BigInteger bigInt = BigInteger.Parse(encryptedChar);
-                char c = (char)(/*(double)*/BigInteger.Divide(bigInt, _privateKey) /*/ tangent*/);
+                char c = (char)(BigInteger.Divide(bigInt, _privateKey));
                 plaintext += c;
             }
 
             return plaintext;
         }
+
     }
 }
